6d41df323c6b1f14700a49203c4984a651693bd3
SantiagoRodriguezP <90514403+SantiagoRodriguezP@users.noreply.github.com>
2023-06-29 16:18:18 -0500

    Archivos a typescript

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(<App />);
+} else {
+  console.error("Element with ID 'root' not found");
+}
