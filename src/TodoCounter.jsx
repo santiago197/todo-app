b6a80726ba194eeddf1638b1d3f3b3628b6a3f45
SantiagoRodriguezP <90514403+SantiagoRodriguezP@users.noreply.github.com>
2023-07-04 13:01:32 -0500

    Mensaje felicitaciones

--- src/TodoCounter.jsx
+++ src/TodoCounter.jsx
@@ -3,12 +3,14 @@ import {Typography} from '@mui/material'
 import { confettiFireworks } from './utils/fireworks'
 
 export const TodoCounter = ({total, completed}) => {
+  let mensaje = `Has completado ${completed} de ${total} TODOS`;
   if(completed===total){
+    mensaje = `¡Felicitaciones!, completaste todas las tareas`;
     confettiFireworks();
   }
   return (
     <Typography variant="body2" color="white" align="center">
-      Has completado {completed} de {total} TODOS
+      {mensaje}
     </Typography>
   )
 }
